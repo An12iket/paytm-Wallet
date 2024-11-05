a5481cfcefa8fd02dd24a30d92524254709fc067
An12iket <Aniketchavan121012@gmail.com>
2024-11-05 12:48:26 +0530

    defined db schema

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin%401234@cluster0.82mjkhz.mongodb.net/');
+
+// Schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+// Created a model 
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+};
