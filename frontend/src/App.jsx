28cc2e4215d731f1ffd31970bb75cb6ce72c7440
An12iket <Aniketchavan121012@gmail.com>
2024-11-20 19:32:03 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
-import Send from "./components/Send";
+import Send from "./components/SendMoney";
 import Dashboard from "./components/Dashboard";
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/signin" element={<Signin/>}></Route>
-        <Route path="/send" element={<Send/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/send" element={<Send/>}></Route>
       </Routes>
       </BrowserRouter>      
     </>
