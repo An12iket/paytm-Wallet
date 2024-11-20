62604d9098237f89f7e84470595098f46fbb9115
An12iket <Aniketchavan121012@gmail.com>
2024-11-20 19:21:21 +0530

    added react-router and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Send from "./components/Send";
+import Dashboard from "./components/Dashboard";
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/signin" element={<Signin/>}></Route>
+        <Route path="/send" element={<Send/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+      </Routes>
+      </BrowserRouter>      
+    </>
   )
 }
 
