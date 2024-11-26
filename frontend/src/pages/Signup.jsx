72ba70a71e8bbdfe28de0e8778c10e6a8e728311
An12iket <Aniketchavan121012@gmail.com>
2024-11-26 10:17:36 +0530

    added components

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,13 +4,15 @@ import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
-import axios from "axios"; 
+import axios from "axios";
+import { useNavigate } from "react-router-dom"
 
 export const Signup = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLasttName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
@@ -20,24 +22,25 @@ export const Signup = () => {
         <InputBox onChange={e => {
           setFirstName(e.target.value);
         }} placeholder="John" label={"First Name"} />
-        <InputBox onChange={e => {
-          setLasttName(e.target.value);
+        <InputBox onChange={(e) => {
+          setLastName(e.target.value);
         }} placeholder="Doe" label={"Last Name"} />
         <InputBox onChange={e => {
-          setUserName(e.target.value);
-        }} placeholder="JohnDoe@gmail.com" label={"Email"} />
-        <InputBox onChange={e => {
-          setPassword(e.target.value);
-        }} 
-        placeholder="John123" label={"Password"} />
+          setUsername(e.target.value);
+        }} placeholder="harkirat@gmail.com" label={"Email"} />
+        <InputBox onChange={(e) => {
+          setPassword(e.target.value)
+        }} placeholder="123456" label={"Password"} />
         <div className="pt-4">
-          <Button onClick={() => {
-            axios.post("http://localhost:3000/api/v1/user/signup", {
-              userName,
-              password,
+          <Button onClick={async () => {
+            const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+              username,
               firstName,
-              lastName
-            })
+              lastName,
+              password
+            });
+            localStorage.setItem("token", response.data.token)
+            navigate("/dashboard")
           }} label={"Sign up"} />
         </div>
         <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
